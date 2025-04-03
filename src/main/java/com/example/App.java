7547bd822a0221640bb51cea04658bee117f200d
Aikansh Boyal <boyalnanun@gmail.com>
2025-04-03 14:11:38 +0530

    chore: added comment to App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,7 @@
 package com.example;
 
 public class App {
+    // Added a comment in App.java
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins CI/CD!");
     }
